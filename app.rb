1bd7a968364ae4aee74cd72895441e21604959a2
Tacobil <Tacobil123@gmail.com>
2026-09-09 09:55:27 +0200

    Anpassade sidor för varje frukt

--- app.rb
+++ app.rb
@@ -5,14 +5,26 @@ class App < Sinatra::Base
     register Sinatra::Reloader
 
     def db
-      return @db if @db
+        return @db if @db
 
-      @db = SQLite3::Database.new(DB_PATH)
-      @db.results_as_hash = true
+        @db = SQLite3::Database.new(DB_PATH)
+        @db.results_as_hash = true
 
-      return @db
+        return @db
     end
 
-    #TODO: Skriv routen hämtar alla frukter i databasen
+    get '/fruits' do
+        @fruits = db.execute('SELECT * FROM products
+                            ORDER BY name ASC')
+        ap @fruits
+        erb(:"fruits/index")
+    end
+
+    get '/fruits/:id' do |id|
+        @fruit = db.execute('SELECT * FROM products WHERE id=?', id).first
+        ap @fruit
+        erb :"fruits/show"
+    end
+    
 
 end
